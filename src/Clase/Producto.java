32e133b82c381e43725a0abe087da66fd7dc1087
CedricPariona <dark.c.b@hotmail.com>
2025-09-21 19:25:48 -0500

    Update Producto.java

--- src/Clase/Producto.java
+++ src/Clase/Producto.java
@@ -11,19 +11,14 @@ public class Producto {
         this.precio = precio;
         this.stock = stock;
     }
-
-<<<<<<< HEAD
-    // Sobrecarga de constructor (solo nombre y precio, stock por defecto en 0)
     public Producto(String nombre, double precio) {
         this(nombre, precio, 0);
-=======
     public Producto(String nombre, double precio) {
-        this(nombre, precio, 0); // stock inicia en 0
+        this(nombre, precio, 0);
     }
 
     public Producto(String nombre) {
-        this(nombre, 0.0, 0); // precio y stock inicial en 0
->>>>>>> bfe3392 (Actualizacion de clase Producto)
+        this(nombre, 0.0, 0);
     }
 
     public String getNombre() {
@@ -39,9 +34,7 @@ public class Producto {
     }
 
     public void vender(int cantidad) {
-<<<<<<< HEAD
         if (cantidad > 0 && cantidad <= stock) {
-=======
         try {
             if (cantidad <= 0) {
                 throw new IllegalArgumentException("La cantidad debe ser mayor a 0.");
@@ -49,7 +42,6 @@ public class Producto {
             if (cantidad > stock) {
                 throw new Exception("No hay suficiente stock de " + nombre);
             }
->>>>>>> bfe3392 (Actualizacion de clase Producto)
             stock -= cantidad;
             System.out.println("Se vendieron " + cantidad + " unidades de " + nombre);
         } catch (IllegalArgumentException e) {
@@ -71,21 +63,16 @@ public class Producto {
 
     public static void main(String[] args) {
         Producto p1 = new Producto("Martillo", 35.5, 10);
-<<<<<<< HEAD
         Producto p2 = new Producto("Clavos", 5.0);
-=======
         Producto p2 = new Producto("Clavos", 2.5);
         Producto p3 = new Producto("Destornillador");
->>>>>>> bfe3392 (Actualizacion de clase Producto)
 
         p1.mostrarInfo();
         p1.vender(3);
         p1.mostrarInfo();
-<<<<<<< HEAD
         p1.vender(15); 
 
         p2.mostrarInfo();
-=======
         p1.vender(15);
 
         System.out.println();
@@ -96,6 +83,5 @@ public class Producto {
         System.out.println();
         p3.mostrarInfo();
         p3.vender(0); // error controlado
->>>>>>> bfe3392 (Actualizacion de clase Producto)
     }
 }
